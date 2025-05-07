89436a67eb4139e1e64c90ef6988b583c73866a6
KasraMg <kasragithub7@gmail.com>
2025-05-07 20:27:36 +0330

    add 3d book

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,36 @@
- import Container from "@/components/modules/container/Container";
+"use client";
+import Container from "@/components/modules/container/Container";
 import { Button } from "@heroui/button";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import { FaCaretRight,FaGithub } from "react-icons/fa";
+import { FaCaretRight, FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
+import dynamic from "next/dynamic";
+
+const BookModel = dynamic(
+  () => import("@/components/modules/bookModel/BookModel"),
+  {
+    ssr: false,
+  },
+);
+
 const page = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <Container>
-      <div className="p-8">
+      <div className="flex justify-evenly rounded-md px-7 py-5 sm:py-16 flex-wrap-reverse"   
+      // style={{
+      //       boxShadow:
+      //         "rgba(0, 0, 0, 0.08) 0px 5px 15px 0px, rgba(25, 28, 33, 0.2) 0px 15px 35px -5px, rgba(0, 0, 0, 0.07) 0px 0px 0px 1px",
+      //     }}
+          >
         <form
           action=""
-          style={{
-            boxShadow:
-              "rgba(0, 0, 0, 0.08) 0px 5px 15px 0px, rgba(25, 28, 33, 0.2) 0px 15px 35px -5px, rgba(0, 0, 0, 0.07) 0px 0px 0px 1px",
-          }}
-          className="w-[400px] px-7 py-6 rounded-md text-center font-thin"
+        
+          className="w-[400px]  text-center font-thin"
         >
           <p className="text-xl font-bold">Login to PenTome</p>
           <p className="text-sm">Welcome back! Please login to continue</p>
@@ -29,7 +44,7 @@ const page = () => {
               variant="light"
             >
               Google
-              <FcGoogle/>
+              <FcGoogle />
             </Button>
             <Button
               className="h-8 rounded-md border-1"
@@ -40,8 +55,8 @@ const page = () => {
               variant="light"
             >
               Github
-              <FaGithub/>
-            </Button> 
+              <FaGithub />
+            </Button>
           </div>
 
           <div>
@@ -76,14 +91,17 @@ const page = () => {
           <div className="mt-4 flex justify-center gap-1 text-sm">
             <p>Secured by</p>
             <Image
-              src={"/whitewithoutlogo.jpg"}
+              src={"/images/Untitled-3-removebg-preview.png"}
               alt="logo"
               className="h-[18px] w-24"
               width={1000}
+              style={{ filter: "hue-rotate(17deg)" }}
               height={1000}
             />
           </div>
         </form>
+
+        <BookModel url="/book.glb" />
       </div>
     </Container>
   );
---
+++ src/components/modules/bookModel/BookModel.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Suspense } from "react";
+
+type ModelProps = {
+  url: string;
+};
+
+function Model({ url }: ModelProps) {
+  const { scene } = useGLTF(url);
+  return (
+    <primitive
+      position={[0, -0.5, 0]}
+      scale={[0.9, 0.9, 0.9]}
+      object={scene}
+      dispose={null}
+      rotation={[0.15, -1.7, 0]}
+    />
+  );
+}
+
+export default function BookModel({ url }: ModelProps) {
+  return (
+    <div className="h-[300px] w-full sm:w-[450px] md:h-[500px] xl:w-[700px]">
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model url={url} />
+        </Suspense>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+    </div>
+  );
+}
